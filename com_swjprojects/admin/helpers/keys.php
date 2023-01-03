c2db7cc7f87c6d10b07d14e0ea5ec5123bcfee88
sdi68 <36002964+sdi68@users.noreply.github.com>
2023-01-03 16:23:05 +0300

    Корректировка набора символов ключа по-умолчанию (убираем 0)

--- com_swjprojects/admin/helpers/keys.php
+++ com_swjprojects/admin/helpers/keys.php
@@ -71,7 +71,7 @@ class SWJProjectsHelperKeys
 			{
 				$characters = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's',
 					't', 'u', 'v', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-					'P', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+					'P', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z',  1, 2, 3, 4, 5, 6, 7, 8, 9);
 			}
 
 			self::$characters = array_unique($characters);
