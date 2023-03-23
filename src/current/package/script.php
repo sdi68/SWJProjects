6f27cb76ba35a2ebcb2f8cb803da9cd0d52ba2b7
sdi68 <36002964+sdi68@users.noreply.github.com>
2023-03-23 15:37:35 +0300

    Убрана установка ECLabs из пакета и перенесена в установку зависимостей в скрипт установки.

--- src/current/package/script.php
+++ src/current/package/script.php
@@ -12,9 +12,15 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
+use Joomla\Registry\Registry;
+use Joomla\CMS\Http\HttpFactory;
+use Joomla\CMS\Installer\Installer;
 use Joomla\CMS\Installer\InstallerAdapter;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\CMS\Version;
+
+
 if (!class_exists('pkg_swjprojectsInstallerScript'))
 {
 	class pkg_swjprojectsInstallerScript
@@ -58,6 +64,25 @@ if (!class_exists('pkg_swjprojectsInstallerScript'))
 			if ($type == 'update')
 			{
 				$this->checkUpdateServer();
+			}
+
+			// Массив расширений, необходимых к установке
+			$dependecies[] = array(
+				'type'    => 'package',  // Тип расширения
+				'element' => 'pkg_eclabs', // Название расширения, как оно зафиксировано #__extensions.element
+				'url'     => "https://econsultlab.ru/uploads/joomla/packages/eclabs/update_eclabs.xml" // URL сервера обновлений расширения
+			);
+			foreach ($dependecies as $dependecy)
+			{
+				$info = $this->getDependency($dependecy);
+				//var_export($info);
+				if ($info)
+				{
+					if ($this->checkInstalled($info))
+					{
+						$this->installDependency($parent, $info['downloads']['downloadurl']);
+					}
+				}
 			}
 
 			return true;
@@ -156,5 +181,215 @@ if (!class_exists('pkg_swjprojectsInstallerScript'))
 				$db->setQuery('UPDATE #__extensions SET package_id = 0 WHERE extension_id = ' . $plugin)->execute();
 			}
 		}
+
+		/**
+		 * Получает информацию о расширении от сервера обновления
+		 *
+		 * @param   array  $dependency  Массив зависимого зависимости
+		 *
+		 * @return false|array
+		 *
+		 * @throws Exception
+		 * @since 1.0.0
+		 */
+		protected function getDependency(array $dependency)
+		{
+			$app        = Factory::getApplication();
+			$version    = new Version;
+			$httpOption = new Registry;
+			$httpOption->set('userAgent', $version->getUserAgent('Joomla', true, false));
+
+			// JHttp transport throws an exception when there's no response.
+			try
+			{
+				$http     = HttpFactory::getHttp($httpOption);
+				$response = $http->get($dependency['url'], array(), 20);
+			}
+			catch (\RuntimeException $e)
+			{
+				$response = null;
+			}
+
+			if ($response === null || $response->code !== 200)
+			{
+				$app->enqueueMessage(Text::sprintf('COM_INSTALLER_MSG_ERROR_CANT_CONNECT_TO_UPDATESERVER', $dependency['url']), 'warning');
+
+				return false;
+			}
+
+			$xml = new SimpleXMLElement($response->body);
+			$ret = json_decode(json_encode($xml), true);
+
+			return $ret['update'] ?? false;
+		}
+
+
+		/**
+		 * Устанавливает расширение по URL
+		 *
+		 * @param           $parent
+		 * @param   string  $url  URL сервера обновлений расширения
+		 *
+		 * @return bool
+		 *
+		 * @throws Exception
+		 * @since 1.0.0
+		 */
+		protected function installDependency($parent, string $url): bool
+		{
+			// Load installer plugins for assistance if required:
+			PluginHelper::importPlugin('installer');
+
+			$app = Factory::getApplication();
+
+			$package = null;
+
+			// This event allows an input pre-treatment, a custom pre-packing or custom installation.
+			// (e.g. from a JSON description).
+			$results = $app->triggerEvent('onInstallerBeforeInstallation', array($this, &$package));
+
+			if (in_array(true, $results, true))
+			{
+				return true;
+			}
+
+			if (in_array(false, $results, true))
+			{
+				return false;
+			}
+
+			// Download the package at the URL given.
+			$p_file = JInstallerHelper::downloadPackage($url);
+
+			// Was the package downloaded?
+			if (!$p_file)
+			{
+				$app->enqueueMessage(Text::_('COM_INSTALLER_MSG_INSTALL_INVALID_URL'), 'error');
+
+				return false;
+			}
+
+			$config   = Factory::getConfig();
+			$tmp_dest = $config->get('tmp_path');
+
+			// Unpack the downloaded package file.
+			$package = JInstallerHelper::unpack($tmp_dest . '/' . $p_file, true);
+
+			// This event allows a custom installation of the package or a customization of the package:
+			$results = $app->triggerEvent('onInstallerBeforeInstaller', array($this, &$package));
+
+			if (in_array(true, $results, true))
+			{
+				return true;
+			}
+
+			if (in_array(false, $results, true))
+			{
+				JInstallerHelper::cleanupInstall($package['packagefile'], $package['extractdir']);
+
+				return false;
+			}
+
+			// Get an installer instance.
+			$installer = new Installer();
+
+			/*
+			 * Check for a Joomla core package.
+			 * To do this we need to set the source path to find the manifest (the same first step as JInstaller::install())
+			 *
+			 * This must be done before the unpacked check because JInstallerHelper::detectType() returns a boolean false since the manifest
+			 * can't be found in the expected location.
+			 */
+			if (is_array($package) && isset($package['dir']) && is_dir($package['dir']))
+			{
+				$installer->setPath('source', $package['dir']);
+
+				if (!$installer->findManifest())
+				{
+					JInstallerHelper::cleanupInstall($package['packagefile'], $package['extractdir']);
+					$app->enqueueMessage(Text::sprintf('COM_INSTALLER_INSTALL_ERROR', '.'), 'warning');
+
+					return false;
+				}
+			}
+
+			// Was the package unpacked?
+			if (!$package || !$package['type'])
+			{
+				JInstallerHelper::cleanupInstall($package['packagefile'], $package['extractdir']);
+				$app->enqueueMessage(Text::_('COM_INSTALLER_UNABLE_TO_FIND_INSTALL_PACKAGE'), 'error');
+
+				return false;
+			}
+
+			// Install the package.
+			if (!$installer->install($package['dir']))
+			{
+				// There was an error installing the package.
+				$msg     = Text::sprintf('COM_INSTALLER_INSTALL_ERROR',
+					Text::_('COM_INSTALLER_TYPE_TYPE_' . strtoupper($package['type'])));
+				$result  = false;
+				$msgType = 'error';
+			}
+			else
+			{
+				// Package installed successfully.
+				$msg     = Text::sprintf('COM_INSTALLER_INSTALL_SUCCESS',
+					Text::_('COM_INSTALLER_TYPE_TYPE_' . strtoupper($package['type'])));
+				$result  = true;
+				$msgType = 'message';
+			}
+
+			// This event allows a custom a post-flight:
+			$app->triggerEvent('onInstallerAfterInstaller', array($parent, &$package, $installer, &$result, &$msg));
+
+			$app->enqueueMessage($msg, $msgType);
+
+			// Cleanup the install files.
+			if (!is_file($package['packagefile']))
+			{
+				$package['packagefile'] = $config->get('tmp_path') . '/' . $package['packagefile'];
+			}
+
+			JInstallerHelper::cleanupInstall($package['packagefile'], $package['extractdir']);
+
+			return $result;
+		}
+
+		/**
+		 * Проверяет возможность и необходимость установки расширения
+		 *
+		 * @param   array  $info  Параметры устанавливаемого расширения
+		 *
+		 * @return bool
+		 *
+		 * @since 1.0.0
+		 */
+		protected function checkInstalled(array $info): bool
+		{
+			// Получаем информацию об установленном расширении
+			$db    = Factory::getDbo();
+			$query = $db->getQuery(true);
+			$query->select($db->quoteName('manifest_cache'))
+				->from($db->qn('#__extensions'))
+				->where($db->quoteName('type') . ' = ' . $db->quote($info['type']))
+				->where($db->quoteName('element') . ' = ' . $db->quote($info['element']));
+			$params = json_decode($db->setQuery($query)->loadResult(), true);
+			//var_dump($params);
+			$ret = false;
+			if (is_null($params))
+			{
+				// Расширение не установлено. Надо устанавливать.
+				$ret = true;
+			}
+			else if (is_array($params))
+			{
+				// Если на сайте более старая версия, то можно устанавливать расширение
+				$ret = (bool) version_compare($info['version'], $params['version'], '>');
+			}
+
+			return $ret;
+		}
 	}
+
 }
