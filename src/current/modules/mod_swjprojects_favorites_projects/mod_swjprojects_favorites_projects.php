bf0b3c41177b30bb099fc5083ed720e59451a092
sdi68 <36002964+sdi68@users.noreply.github.com>
2023-06-21 14:36:40 +0300

    Выпуск версии 2.0.1. Изменена ссылка на сервер обновлений, добавлен функционал базового проекта, скорректирован вывод заголовка таблицы.

--- src/current/modules/mod_swjprojects_favorites_projects/mod_swjprojects_favorites_projects.php
+++ src/current/modules/mod_swjprojects_favorites_projects/mod_swjprojects_favorites_projects.php
@@ -1,25 +1,26 @@
 <?php
 /*
- * @package    SW JProjects Payment
- * @subpackage module mod_swjprojects_favorites_projects
- * @version    1.0.0
- * @author     Econsult lab - https://econsultlab.ru
- * @copyright  Copyright (c) 2022 Econsult Lab. All rights reserved.
+ * @package    SWJProjects Component
+ * @subpackage    mod_swjprojects_favorite_projects
+ * @version    2.0.1
+ * @author Econsult Lab.
+ * @based on   SW JProjects Septdir Workshop - www.septdir.com
+ * @copyright  Copyright (c) 2023 Econsult Lab. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
- *  @link       https://econsultlab.ru/
+ * @link       https://econsultlab.ru
  */
 
 /**
  *
  * The module mod_swjprojects_favorite_projects file
- * @package pkg_swjprojects_payments
+ * @package     pkg_swjprojects_payments
  * @subpackage  mod_swjprojects_favorite_projects
- * @version 1.0.0
- * @since 1.0.0
+ * @version     2.0.1
+ * @since       1.0.0
  *
  * Variable definitions
  * @var Joomla\Registry\ $params
- * @var ModuleHelper $module
+ * @var ModuleHelper     $module
  */
 defined('_JEXEC') or die;
 
@@ -27,6 +28,7 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 use Joomla\CMS\Table\Table;
+use Joomla\Registry\Registry;
 
 require_once __DIR__ . '/helper.php';
 
@@ -43,43 +45,71 @@ $use_cat_filter = $params->get('use_cat_filter', false);
 // Prepare model
 BaseDatabaseModel::addIncludePath(JPATH_SITE . '/components/com_swjprojects/models');
 Table::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_swjprojects/tables');
-$items = array();
+$items    = array();
 $projects = $params->get('projects', null);
-if ($use_cat_filter) {
+if ($use_cat_filter)
+{
 
-    $model = BaseDatabaseModel::getInstance('Projects', 'SWJProjectsModel', array('ignore_request' => true));
-    $model->setState('category.id', $params->get('category', 1));
-    $model->setState('params', Factory::getApplication()->getParams());
-    $model->setState('filter.published', 1);
-    $model->setState('list.limit', $params->get('limit', 0));
-    $model->setState('list.start', 0);
+	$model = BaseDatabaseModel::getInstance('Projects', 'SWJProjectsModel', array('ignore_request' => true));
+	$model->setState('category.id', $params->get('category', 1));
+	$model->setState('params', Factory::getApplication()->getParams());
+	$model->setState('filter.published', 1);
+	$model->setState('list.limit', $params->get('limit', 0));
+	$model->setState('list.start', 0);
 
-    $ordering = $params->get('ordering');
+	$ordering = $params->get('ordering');
 
-    if ($ordering === 'rand()') {
-        $model->setState('list.ordering', Factory::getDbo()->getQuery(true)->Rand());
-    } else {
-        $direction = $params->get('direction', 1) ? 'DESC' : 'ASC';
-        $model->setState('list.direction', $direction);
-        $model->setState('list.ordering', $ordering);
-    }
-    // Get items
-    $tmps = $model->getItems();
-    if (is_array($tmps)) {
-        foreach ($tmps as $tmp) {
-            if (in_array($tmp->id, $projects)) {
-                $items[] = $tmp;
-            }
-        }
-    }
-} else {
-    $model = BaseDatabaseModel::getInstance('Project', 'SWJProjectsModel', array('ignore_request' => true));
-    $model->setState('params', Factory::getApplication()->getParams());
-    if (is_array($projects)) {
-        foreach ($projects as $project) {
-            $items[] = $model->getItem($project);
-        }
-    }
+	if ($ordering === 'rand()')
+	{
+		$model->setState('list.ordering', Factory::getDbo()->getQuery(true)->Rand());
+	}
+	else
+	{
+		$direction = $params->get('direction', 1) ? 'DESC' : 'ASC';
+		$model->setState('list.direction', $direction);
+		$model->setState('list.ordering', $ordering);
+	}
+	// Get items
+	$tmps = $model->getItems();
+	if (is_array($tmps))
+	{
+		foreach ($tmps as $tmp)
+		{
+			if (in_array($tmp->id, $projects))
+			{
+				/**
+				 * Выводим только базовые или самостоятельные проекты
+				 * @since 2.0.1
+				 */
+				$params       = new Registry($tmp->projects->params);
+				$base_project = $params->get('base_project', '');
+				if ($base_project)
+					continue;
+				$items[] = $tmp;
+			}
+		}
+	}
+}
+else
+{
+	$model = BaseDatabaseModel::getInstance('Project', 'SWJProjectsModel', array('ignore_request' => true));
+	$model->setState('params', Factory::getApplication()->getParams());
+	if (is_array($projects))
+	{
+		foreach ($projects as $project)
+		{
+			/**
+			 * Выводим только базовые или самостоятельные проекты
+			 * @since 2.0.1
+			 */
+			$tmp          = $model->getItem($project);
+			$params       = new Registry($tmp->projects->params);
+			$base_project = $params->get('base_project', '');
+			if ($base_project)
+				continue;
+			$items[] = $tmp;
+		}
+	}
 }
 
 // Show module
